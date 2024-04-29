5f70c15b367c3386773c5e6ef8dfc8fe6709f6a3
BrayanCifuentes <bcifuentes@demosoft.com.gt>
2024-04-29 13:55:07 -0600

    agregando api al repo

---
+++ ApiQueries/ApiQueries/Controllers/Ctrl_VerificarArchivos.cs
@@ -0,0 +1,67 @@
+﻿using ApiQueries.Connection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+
+namespace ApiQueries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ctrl_VerificarArchivos : ControllerBase
+    {
+        [HttpPost()]
+        public IActionResult VerificarArchivos([FromBody] List<string> fileNames)
+        {
+            if (fileNames == null || fileNames.Count == 0)
+            {
+                return BadRequest("No se han proporcionado nombres de archivos.");
+            }
+
+            Conexion cn = new Conexion();
+
+            try
+            {
+                // Filtrar solo los archivos que terminen en .sql
+                var archivosEnFolder = fileNames.Where(fileName => fileName.EndsWith(".sql")).ToList();
+
+                List<string> archivosGuardados = new List<string>();
+                using (SqlConnection sqlConnection = new SqlConnection(cn.cadenaSQL()))
+                {
+                    sqlConnection.Open();
+                    string sqlQuery = "SELECT Archivo FROM tbl_A_Actualizacion ORDER BY Actualizacion DESC";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, sqlConnection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                archivosGuardados.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+
+                // Obtener archivos existentes y faltantes
+                List<string> archivosExistentes = archivosEnFolder.Intersect(archivosGuardados).ToList();
+                List<string> archivosFaltantes = archivosEnFolder.Except(archivosGuardados).ToList();
+
+                var respuesta = new
+                {
+                    ArchivosGuardados = archivosExistentes,
+                    ArchivosFaltantes = archivosFaltantes
+                };
+
+                return Ok(respuesta);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al verificar archivos: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ApiQueries/ApiQueries/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:52933")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+// Configure JWT authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
