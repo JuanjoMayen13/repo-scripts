50937c3c93bc70dff7eaa08085e3bc5bfe9f24a3
BrayanCifuentes <bcifuentes@demosoft.com.gt>
2024-04-29 07:48:43 -0600

    Agregando archivos

--- prueba-scripts-sql/src/app/app.component.ts
+++ prueba-scripts-sql/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablaScriptsComponent } from './tabla-scripts/tabla-scripts.component';
+import * as FilePond from 'filepond';
+import 'filepond/dist/filepond.css';
+import 'filepond-plugin-file-validate-size';
+import 'filepond-plugin-file-validate-type';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TablaScriptsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
