242ad4dd576576667e469a1a508fd632e3ffb176
BrayanCifuentes <bcifuentes@demosoft.com.gt>
2024-05-02 09:24:39 -0600

    agregando actualizaciones en api ctrl_verificarArchivos

--- ApiQueries/ApiQueries/Controllers/Ctrl_VerificarArchivos.cs
+++ ApiQueries/ApiQueries/Controllers/Ctrl_VerificarArchivos.cs
@@ -29,26 +29,53 @@ namespace ApiQueries.Controllers
                 // Filtrar solo los archivos que terminen en .sql
                 var archivosEnFolder = fileNames.Where(fileName => fileName.EndsWith(".sql")).ToList();
 
-                List<string> archivosGuardados = new List<string>();
+                var archivosVerificados = new List<dynamic>(); // Cambio object a dynamic
                 using (SqlConnection sqlConnection = new SqlConnection(cn.cadenaSQL()))
                 {
                     sqlConnection.Open();
-                    string sqlQuery = "SELECT Archivo FROM tbl_A_Actualizacion ORDER BY Actualizacion DESC";
+                    string sqlQuery = "SELECT Archivo, Fecha_Hora, UserName, Descripcion FROM tbl_A_Actualizacion ORDER BY Actualizacion DESC";
                     using (SqlCommand command = new SqlCommand(sqlQuery, sqlConnection))
                     {
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                archivosGuardados.Add(reader.GetString(0));
+                                string archivo = reader.GetString(0);
+                                string fechaHora = reader.IsDBNull(1) ? "" : reader.GetDateTime(1).ToString(); 
+                                string userName = reader.IsDBNull(2) ? "" : reader.GetString(2); 
+                                string descripcion = reader.IsDBNull(3) ? "" : reader.GetString(3); 
+
+                                var archivoInfo = new
+                                {
+                                    Archivo = archivo,
+                                    Fecha_Hora = fechaHora,
+                                    UserName = userName,
+                                    Descripcion = descripcion
+                                };
+
+                                archivosVerificados.Add(archivoInfo);
                             }
                         }
                     }
                 }
 
-                // Obtener archivos existentes y faltantes
-                List<string> archivosExistentes = archivosEnFolder.Intersect(archivosGuardados).ToList();
-                List<string> archivosFaltantes = archivosEnFolder.Except(archivosGuardados).ToList();
+                // Filtrar archivos encontrados y faltantes
+                var archivosExistentes = archivosVerificados.Where(archivo => archivosEnFolder.Contains(archivo.Archivo)).Select(archivo => new
+                {
+                    Archivo = archivo.Archivo,
+                    Fecha_Hora = archivo.Fecha_Hora,
+                    UserName = archivo.UserName,
+                    Descripcion = archivo.Descripcion
+                }).ToList();
+
+                var archivosFaltantes = archivosEnFolder.Except(archivosExistentes.Select(archivo => archivo.Archivo)).Select(archivo => new
+                {
+                    Archivo = archivo,
+                    Fecha_Hora = "",
+                    UserName = "",
+                    Descripcion = ""
+                }).ToList();
+
 
                 var respuesta = new
                 {
